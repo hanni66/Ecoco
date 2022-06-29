bdd2605c69e79aa6fe61051abd18af2228e5a8df
hanni66 <haeunkim0807@naver.com>
2022-06-29 22:03:11 +0900

    프로젝트 파일 생성

---
+++ ECOCO/ECOCO/ECOCOApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECOCOApp.swift
+//  ECOCO
+//
+//  Created by 김하은 on 2022/06/29.
+//
+
+import SwiftUI
+
+@main
+struct ECOCOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
